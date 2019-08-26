85f92f34351a74962401cfb689a6212ddee8bb2d
Albert (On Ivan's PC) <kivanol@ufamail.ru>
2019-08-27 02:36:27 +0500

    added geoip

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express();
+const geoip = require('geoip-lite');
 app.use(express.static('static'));
 app.set('view engine', 'pug');
+app.set('trust proxy', true)
 app.get('/', function (req, res) {
     const headers = req.headers;
     headers.userAgent = headers['user-agent'];
@@ -9,6 +11,10 @@ app.get('/', function (req, res) {
     headers.acceptEncoding = headers['accept-encoding'];
     headers.ifNoneMatch = headers['if-none-match'];
 
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip);
+    const gip = geoip.lookup(ip);
+    console.log(gip);
     res.render('index', {headers});
 });
 
